6fb307a5d26621576ea9250d31fa663ef1d6c086
Parth <vavaliyaparth@gmail.com>
2023-09-11 19:07:03 +0530

    first add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import TopNav from './Components/TopNav';
+import CatNav from './Components/CatNav';
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './Components';
+import ProductDetails from './Components/ProductDetails';
+import Cart from './Components/Cart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopNav/>
+      <CatNav/>
+      
+      <Routes>
+        <Route path='/' Component={LandingPage}/>
+        <Route path='/productDetails' Component={ProductDetails}/>
+        <Route path='/cart' Component={Cart}/>
+      </Routes>
+
     </div>
   );
 }
